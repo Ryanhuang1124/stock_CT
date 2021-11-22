924ad3a2f3f8479c68c84bac032ddfe3e349a8bd
Ryan <ryanhuang1124@gmail.com>
2021-11-22 21:14:28 +0800

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:stock_ct_web/toolbox.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-
-void main()async {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
   runApp(const MyApp());
 }
 
@@ -34,41 +35,210 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future<DocumentSnapshot>? getDeviceTokenList;
+  Map<String, String> groupList = {};
   TextEditingController testController = TextEditingController();
   String? inputText;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  @override
+  void initState() {
+    getDeviceTokenList = firestore.collection('DeviceToken').doc('token').get();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
+      backgroundColor: MyColor.appThemeBackground,
       appBar: AppBar(
-        title: const Center(child: Text('承通投顧')),
+        backgroundColor: MyColor.appThemeBackground,
+        elevation: 0,
+        title: Center(
+            child: Text(
+          '承通投顧',
+          style: MyText.myTextStyle(fontSize: 23, fontColor: MyColor.textWhite),
+        )),
       ),
-      body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              '請輸入測試推播訊息:',
-              style: TextStyle(fontSize: 22),
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              controller: testController,
-              onChanged: (newValue){
-                inputText=newValue;
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            flex: 9,
+            child: FutureBuilder<DocumentSnapshot>(
+              future: getDeviceTokenList,
+              builder: (ctx, snapshot) {
+                if (snapshot.hasData) {
+                  Map<String, dynamic> deviceList =
+                      snapshot.data!.data() as Map<String, dynamic>;
+                  return Row(
+                    children: [
+                      Expanded(
+                        flex: 8,
+                        child: Column(
+                          children: [
+                            Expanded(
+                              flex: 1,
+                              child: Text(
+                                '裝置列表',
+                                style: MyText.myTextStyle(
+                                    fontSize: 22, fontColor: MyColor.textWhite),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 9,
+                              child: ListView.builder(
+                                itemCount: deviceList.length,
+                                shrinkWrap: true,
+                                itemBuilder: (ctx, i) {
+                                  return Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                          flex: 3,
+                                          child: Text(
+                                            deviceList.keys.toList()[i],
+                                            style: MyText.myTextStyle(
+                                                fontSize: 22,
+                                                fontColor: MyColor.textWhite),
+                                          ),
+                                        ),
+                                        Expanded(
+                                          flex: 7,
+                                          child: Text(
+                                            deviceList[
+                                                deviceList.keys.toList()[i]],
+                                            style: MyText.myTextStyle(
+                                              fontSize: 22,
+                                              fontColor: MyColor.textWhite,
+                                            ),
+                                          ),
+                                        ),
+                                        Expanded(
+                                          flex: 2,
+                                          child: IconButton(
+                                            icon: const Icon(
+                                              Icons.add_circle_outline,
+                                              color: Colors.white,
+                                            ),
+                                            onPressed: () {
+                                              setState(() {
+                                                groupList[deviceList.keys
+                                                        .toList()[i]] =
+                                                    deviceList[deviceList.keys
+                                                        .toList()[i]];
+                                              });
+                                            },
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                },
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Column(
+                          children: [
+                            Expanded(
+                              flex: 1,
+                              child: Text(
+                                '群組',
+                                style: MyText.myTextStyle(
+                                    fontSize: 22, fontColor: MyColor.textWhite),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 9,
+                              child: ListView.builder(
+                                  itemCount: groupList.length,
+                                  itemBuilder: (ctx, i) {
+                                    return Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            groupList.keys.toList()[i],
+                                            style: MyText.myTextStyle(
+                                                fontSize: 22,
+                                                fontColor: MyColor.textWhite),
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  }),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                } else {
+                  return const Center(
+                    child: CupertinoActivityIndicator(
+                      radius: 20,
+                    ),
+                  );
+                }
               },
             ),
-            const SizedBox(height: 20,),
-            ElevatedButton(onPressed: ()async {
-              await MyDIO.fcmSend(title: '測試訊息',body: testController.text);
-
-            }, child: const Text(
-              '發送',
-              style: TextStyle(fontSize: 18),
-            ),),
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Expanded(
+            flex: 4,
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    '請輸入測試推播訊息:',
+                    style: MyText.myTextStyle(
+                        fontSize: 22, fontColor: MyColor.textWhite),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    controller: testController,
+                    style: MyText.myTextStyle(
+                        fontSize: 22, fontColor: MyColor.textWhite),
+                    onChanged: (newValue) {
+                      inputText = newValue;
+                    },
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      await MyDIO.fcmSend(
+                          title: '測試訊息',
+                          body: testController.text,
+                          target: groupList.values.toList());
+                    },
+                    child: const Text(
+                      '發送',
+                      style: TextStyle(fontSize: 22),
+                    ),
+                  ),
+                ),
+                const Expanded(
+                  child: SizedBox(),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/toolbox.dart
+++ lib/toolbox.dart
@@ -1,41 +1,71 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 
-
-
-class MyDIO{
-
-  static Future<bool> fcmSend({required String title,required String body}) async {
-    String serverKey ='AAAAPLHzgPY:APA91bHJXCUlKpBDZ5fDlo8g6iRwz9m8WEmXm_WI5p6MxAohWZoAGCm9KKs0GxaHcENwg7Ac48GwRntT34uwSCjpixKtfP36x5ZImgAfyEgAiSX91-iD3BR2W_lIXgipYdpNJ-Ok2-WW';
+class MyDIO {
+  static Future<bool> fcmSend(
+      {required String title,
+      required String body,
+      required List<String> target}) async {
+    String serverKey =
+        'AAAAPLHzgPY:APA91bHJXCUlKpBDZ5fDlo8g6iRwz9m8WEmXm_WI5p6MxAohWZoAGCm9KKs0GxaHcENwg7Ac48GwRntT34uwSCjpixKtfP36x5ZImgAfyEgAiSX91-iD3BR2W_lIXgipYdpNJ-Ok2-WW';
     String uri = "https://fcm.googleapis.com/fcm/send";
-    String token = 'dMwmtG3sRLuJLbWKnJaEnH:APA91bFbvWnysCT2WmvQcMADdXtLYRc3B0dbetcWXjjx3gnM5Vdu7RAB6t0a_-QXGFfb9OcdlDRzxT5ajyoi6lHi5qLauHKgVAceONEGvs-AxTrrfHeJu889UsFYH_OLbqouXR7SIdA9';
-    var jsonData =json.encode({
-      "to": token,
-      "notification":{
-        "title": title,
-        "body": body,
-      }
-    });
+    var jsonData;
+    if (target.length > 1) {
+      jsonData = json.encode({
+        "registration_ids": target,
+        "notification": {
+          "title": title,
+          "body": body,
+        }
+      });
+    } else {
+      jsonData = json.encode({
+        "to": target[0],
+        "notification": {
+          "title": title,
+          "body": body,
+        }
+      });
+    }
 
+    var response = await Dio().post(uri,
+        data: jsonData,
+        options: Options(
+            headers: {
+              HttpHeaders.authorizationHeader: "key=$serverKey",
+            },
+            contentType: 'application/json',
+            validateStatus: (status) {
+              print(status);
+              return true;
+            }));
 
-      var response = await Dio().post(uri,
-          data: jsonData,
-          options: Options(
-              headers: {
-                HttpHeaders.authorizationHeader: "key=$serverKey",
-              },
-              contentType: 'application/json',
-              validateStatus: (status) {
-                print(status);
-                return true;
-              }));
-
-      if(response.statusCode==200){
-        return true;
-      }else{
-        return false;
-      }
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
   }
+}
 
+class MyColor {
+  static Color appThemeBackground = const Color(0xFF303455);
+  static Color textWhite = Colors.white;
+}
+
+class MyText {
+  static TextStyle myTextStyle(
+      {required double fontSize,
+      FontWeight? weight,
+      String? fontFamily,
+      Color? fontColor}) {
+    return TextStyle(
+      fontSize: fontSize,
+      fontFamily: fontFamily ?? 'NotoSansTC',
+      fontWeight: weight ?? FontWeight.normal,
+      color: fontColor ?? Colors.black,
+    );
+  }
 }
